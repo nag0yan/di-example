0b8a708c6ee30f29a9e555d404fb01e3492df68f
nag0yan <matsui-ka@kadokawa.jp>
2024-02-19 23:15:57 +0900

    mocked db(TODO: get value from sqlite)

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/nag0yan/di-example
 
 go 1.21.5
 
-require github.com/mattn/go-sqlite3 v1.14.22 // indirect
+require github.com/mattn/go-sqlite3 v1.14.22
--- good_test.go
+++ good_test.go
@@ -1,9 +1,18 @@
 package main
 
-import "testing"
+import (
+	"database/sql"
+	"log"
+	"testing"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 type testGoodX1 struct{}
 type testGoodX2 struct{}
+type testGoodXDB struct{
+	db *sql.DB
+}
 
 func (x testGoodX1) hello() string {
 	return "This is test"
@@ -11,6 +20,10 @@ func (x testGoodX1) hello() string {
 
 func (x testGoodX2) hello() string {
 	return "This is another test"
+}
+
+func (x testGoodXDB) hello() string {
+	return "This is db"
 }
 
 func TestHelloFromGoodX(t *testing.T) {
@@ -29,6 +42,26 @@ func TestHelloFromGoodX(t *testing.T) {
 	given = testGoodX2{}
 	got = helloFromGoodX(given)
 	want = "This is another test world"
+	if got != want {
+		t.Errorf("got %v, but want %v", got, want)
+	}
+}
+
+func TestHelloFromDB(t *testing.T) {
+	db, err := sql.Open("sqlite3", "./example.sql")
+	if err != nil {
+		log.Fatal("failed to connect db: ", err)
+	}
+	defer db.Close()
+
+	var (
+		given IGoodX
+		got   string
+		want  string
+	)
+	given = testGoodXDB{db: db}
+	got = helloFromGoodX(given)
+	want = "This is db world"
 	if got != want {
 		t.Errorf("got %v, but want %v", got, want)
 	}
